8d3cea0dbfb8e53363b631a56ee75de8f40a5e85
SerhiiOberemchuk <gardenitparts@gmail.com>
2023-09-30 21:53:28 +0200

    task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const boxForImg = document.querySelector(".gallery");
+const markup = galleryItems
+  .map(
+    (item) =>
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`
+  )
+  .join("");
+
+boxForImg.innerHTML = markup;
+
+boxForImg.addEventListener("click", onclick);
+function onclick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+  //   console.log(lightbox);
+}
